2be647180f9de923c1f395372c2a952ce32ef083
Benjamin Pearce <hawk.git@bcpearce.com>
2015-02-22 02:40:52 -0500

    adjusted coloring

--- app/models/median_rent.rb
+++ app/models/median_rent.rb
@@ -13,7 +13,7 @@ class MedianRent < ActiveRecord::Base
   end
 
   def self.assign_ranks!(ranks=8)
-    interval = (self.max_rent-self.min_rent)/ranks
+    interval = 300
     (1..ranks).each do |r|
       mrs = MedianRent.where("rent >= #{min_rent+(r-1)*interval}")
       mrs.update_all(rank: r)
