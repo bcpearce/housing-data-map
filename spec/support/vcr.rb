20c9e81998fee13f2347bfaddb1a8fa4517e76d0
Benjamin Pearce <hawk.git@bcpearce.com>
2015-02-22 11:19:42 -0500

    purged quandl api key

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -3,4 +3,5 @@ VCR.configure do |c|
   c.hook_into :webmock
   c.configure_rspec_metadata!
   c.default_cassette_options = { :record => :new_episodes }
+  c.filter_sensitive_data("<QUANDL_API_KEY>") { ENV["QUANDL_API_KEY"] }
 end
